4aa2dbad2ff067c1de91c10b751205bef7481fef
José Monte <josecmontejr@gmail.com>
2020-02-13 08:45:10 -0300

    master | atualizacao de dependencias e correcao dos plugins do postcss

---
+++ README.md
@@ -0,0 +1,11 @@
+## Como baixar o projeto:
+
+```console
+$ git clone https://github.com/jmontejr/boilerplate_gulp.git
+```
+
+## Instalar dependências:
+
+```console
+$ npm install
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 const gclean = require('gulp-clean');
 const gsass = require('gulp-sass');
-const clean_css = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 const uglifyes = require('uglify-es');
 const composer = require('gulp-uglify/composer');
 const uglify = composer(uglifyes, console);
@@ -14,41 +15,54 @@ const imagemin = require('gulp-imagemin');
 const { gifsicle, mozjpeg, optipng, svgo } = imagemin;
 const cache = require('gulp-cache');
 
-const fs = require('fs');
-const INIT_PATH = './src/';
-const DEST_PATH = './dist/';
-const MAP_PATH = './sourcemap/'
+const pathExists = require('path-exists');
+const INIT_PATH = './src';
+const DEST_PATH = './dist';
+const MAP_PATH = '.'
 
+
+// Swallow error
 function swallowError(error) {
     console.log(error.toString());
     this.emit("end");
 }
 
+// Clean dist
 function cleanDist() {
     return src(DEST_PATH)
         .pipe(gclean());
 }
 
+// Task functions
 function sass() {
-    return src(`${INIT_PATH}assets/scss/style.scss`)
+    const plugins = [
+        autoprefixer(),
+        cssnano()
+    ];
+
+    return src(`${INIT_PATH}/assets/scss/style.scss`)
         .pipe(sourcemaps.init())
         .pipe(gsass())
-        .pipe(autoprefixer())
         .on('error', swallowError)
-        .pipe(clean_css())
+        .pipe(postcss(plugins))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(sourcemaps.write(MAP_PATH))
-        .pipe(dest(`${DEST_PATH}css`));
+        .pipe(dest(`${DEST_PATH}/css`));
 }
 
 function html() {
-    return src(`${INIT_PATH}index.html`)
+    return src(`${INIT_PATH}/index.html`)
         .pipe(htmlmin({ collapseWhitespace: true }))
         .on('error', swallowError)
         .pipe(dest(DEST_PATH));
 }
 
 function image() {
-    return src(['', ''])
+    return src([
+        `${INIT_PATH}/images/*.*`,
+        `${INIT_PATH}/images/*/*.*`,
+        `${INIT_PATH}/images/*/*/*.*`
+    ])
         .pipe(cache(imagemin([
             gifsicle({ interlaced: true }),
             mozjpeg({ quality: 75, progressive: true }),
@@ -61,54 +75,58 @@ function image() {
             })
         ])))
         .on('error', swallowError)
-        .pipe(dest(`${DEST_PATH}images`));
+        .pipe(dest(`${DEST_PATH}/images`));
 }
 
 function fonts() {
     return src([
-            `${INIT_PATH}assets/fonts/lato`,
-            `${INIT_PATH}assets/fonts/montserrat`    
-        ])
-        .pipe(dest(`${DEST_PATH}fonts`));
+        `${INIT_PATH}/assets/fonts/*.*`,
+        `${INIT_PATH}/assets/fonts/*/*.*`,
+        `${INIT_PATH}/assets/fonts/*/*.*.*`
+    ])
+        .pipe(dest(`${DEST_PATH}/fonts`));
 }
 
 function scripts() {
     return src([
-            `${INIT_PATH}assets/js/*.js`,
-            `${INIT_PATH}assets/js/*.*.js`
-        ])
-        .pipe(sourcemaps.init())
+        `${INIT_PATH}/assets/js/*.js`,
+        `${INIT_PATH}/assets/js/*.*.js`
+    ])
+        .pipe(concat('index.js'))
         .pipe(uglify())
         .on('error', swallowError)
-        .pipe(concat('index.js'))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write(MAP_PATH))
-        .pipe(dest(`${DEST_PATH}js`));
+        .pipe(dest(`${DEST_PATH}/js`));
 }
 
+// Watch files
 function watchFiles() {
     watch([
-        `${INIT_PATH}assets/scss/*.scss`,
-        `${INIT_PATH}assets/scss/*/*.scss`
+        `${INIT_PATH}/assets/scss/*.scss`,
+        `${INIT_PATH}/assets/scss/*/*.scss`
     ], sass);
     watch([
-        `${INIT_PATH}assets/images/*.*`,
-        `${INIT_PATH}assets/images/*/*.*`
+        `${INIT_PATH}/assets/images/*.*`,
+        `${INIT_PATH}/assets/images/*/*.*`
     ], image);
     watch(`${INIT_PATH}`, html);
     watch([
-        `${INIT_PATH}assets/js/*.js`,
-        `${INIT_PATH}assets/js/*.*.js`,
-        `${INIT_PATH}assets/js/*/*.js`,
-        `${INIT_PATH}assets/js/*/*.*.js`,
+        `${INIT_PATH}/assets/js/*.js`,
+        `${INIT_PATH}/assets/js/*.*.js`,
+        `${INIT_PATH}/assets/js/*/*.js`,
+        `${INIT_PATH}/assets/js/*/*.*.js`,
     ], scripts);
 }
 
+// define complex tasks
 const clean = series(cleanDist);
 const develop = parallel(fonts, sass, image, html, scripts);
-const dist_exist = fs.exists(DEST_PATH, (exists) => exists);
+const build = () => {
+    return pathExists.sync(DEST_PATH) ? series(clean, develop) : develop;
+}
 
+// export tasks
 exports.clean = clean;
 exports.dev = series(develop, watchFiles);
 exports.default = develop;
-exports.build = dist_exist ? series(clean, develop) : develop;
+exports.build = build();
